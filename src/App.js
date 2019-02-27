73966c7664a15787c43e9a36d63586d9a7f0b929
Shorasul <shorasul@Shorasuls-MacBook-Pro.local>
2019-02-27 16:15:57 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default";
+import {Switch, Route} from 'react-router-dom';
+import Modal from "./components/Modal";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+           <Fragment>
+               <NavBar/>
+               <Switch>
+                   <Route path={'/'} exact component={ProductList}/>
+                   <Route path={'/details'} component={Details}/>
+                   <Route path={'/cart'} component={Cart}/>
+                   <Route  component={Default}/>
+               </Switch>
+               <Modal/>
+           </Fragment>
+        );
+    }
 }
 
 export default App;
